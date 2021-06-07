0f2c2381ef367ec0c600a5081f684567da7fde8a
yashsehgal <yashsehgal.work@gmail.com>
2021-06-07 16:06:29 +0530

    button components props added

--- src/lib/jsx/src/Components/Buttons/PrimaryButton3D.jsx
+++ src/lib/jsx/src/Components/Buttons/PrimaryButton3D.jsx
@@ -4,8 +4,22 @@ function PrimaryButton3D(props) {
   
   const _CLASS_LIST = "gbtn-3d-primary " + `${props.Animate}` + " " + `${props.ShadowEffect}`;
   
+  const _STYLE_LIST = {
+    backgroundColor: props.BGColor,
+    color: props.TextColor,
+    borderRadius: props.BorderRadius,
+    padding: props.Padding,
+    boxShadow: props.BoxShadow,
+    outline: props.Outline
+  }
+
   return (
-    <button className={_CLASS_LIST}>{props.TextContent}</button>
+    <button 
+      className={_CLASS_LIST}
+      style={_STYLE_LIST}
+      >
+        {props.TextContent}
+      </button>
   )
 }
 
